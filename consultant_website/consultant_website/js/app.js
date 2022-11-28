dab7b817fc4bc81e3002859a173145d5bbb454bd
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-27 20:23:57 -0600

    Navbar Transparent Transition Effect

--- consultant_website/consultant_website/js/app.js
+++ consultant_website/consultant_website/js/app.js
@@ -3,3 +3,26 @@ const year = new Date().getFullYear();
 
 
 currentYear.innerText = year;
+
+const navbar = document.getElementById('navbar');
+let scrolled = false;
+window.onscroll = function(){
+  if(window.scrollY > 100){
+    navbar.classList.remove('top');
+    if(!scrolled) {
+      navbar.style.transform = 'translateY(-70px)';
+    }
+    setTimeout(function() {
+      navbar.style.transform = 'translateY(0px)';
+      scrolled = true;
+    }, 200);
+  } else {
+    navbar.classList.add('top');
+    scrolled = false;
+  }
+}
+
+
+
+
+ 
